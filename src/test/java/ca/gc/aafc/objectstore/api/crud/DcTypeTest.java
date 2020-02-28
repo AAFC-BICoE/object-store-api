f40a8ea52d12b666fd22d9f94841e78ad5410f0e
Shemy Gan <xuemei.gan@canada.ca>
2020-02-28 09:06:48 -0500

    Bug #18801 When saving moving image dctype,api throws DcType must not         be null exception, removed unused annotations.

--- src/test/java/ca/gc/aafc/objectstore/api/crud/DcTypeTest.java
+++ src/test/java/ca/gc/aafc/objectstore/api/crud/DcTypeTest.java
@@ -5,17 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import ca.gc.aafc.objectstore.api.ObjectStoreApiLauncher;
 import ca.gc.aafc.objectstore.api.entities.DcType;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = ObjectStoreApiLauncher.class)
-@ActiveProfiles("test")
+
 public class DcTypeTest {
   
   private DcType testDcType= DcType.MOVING_IMAGE;
