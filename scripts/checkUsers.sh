8fd51e791de15de7d10d567565a688d82ceaf3e3
Chalker, Tom <tom.chalker@canada.ca>
2020-03-02 11:26:05 -0500

    Simplified script

--- scripts/checkUsers.sh
+++ scripts/checkUsers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PGPASSWORD="$POSTGRES_PASSWORD"
-echo "application = $APPLICATION, database = $POSTGRES_DB, schema = $(echo '$spring.liquibase.defaultSchema' | awk  -f envSubstitution.awk)"
+echo 'application = $APPLICATION, database = $POSTGRES_DB, schema = $spring.liquibase.defaultSchema' | awk  -f envSubstitution.awk
 ./executeSQL.sh "SELECT id, process, status, lastModified FROM public.DatabaseReadiness WHERE application = '$APPLICATION' ORDER BY lastModified DESC LIMIT 1;"
 STATUS=$(./executeSQL.sh "SELECT status FROM public.DatabaseReadiness WHERE application = '$APPLICATION' ORDER BY lastModified DESC LIMIT 1;")
 
@@ -8,16 +8,13 @@ if [ "$STATUS" = "3" ]; then
    exit
 else
    echo "CREATE DATABASE $POSTGRES_DB;" | psql  -h $POSTGRES_HOST -d object_store -U $POSTGRES_USER
-   awk -f envSubstitution.awk createUsers.sql
-   awk -f envSubstitution.awk createUsers.sql | psql  -h $POSTGRES_HOST -d $POSTGRES_DB -U $POSTGRES_USER
+   awk -f envSubstitution.awk createUsers.sql | tee | psql  -h $POSTGRES_HOST -d $POSTGRES_DB -U $POSTGRES_USER
    
    export PGPASSWORD=$(echo '$spring.liquibase.password' | awk  -f envSubstitution.awk)
-   awk -f envSubstitution.awk createSchema.sql
-   awk -f envSubstitution.awk createSchema.sql | psql -h $POSTGRES_HOST -d $POSTGRES_DB -U $(echo '$spring.liquibase.user' | awk  -f envSubstitution.awk)
+   awk -f envSubstitution.awk createSchema.sql | tee | psql -h $POSTGRES_HOST -d $POSTGRES_DB -U $(echo '$spring.liquibase.user' | awk  -f envSubstitution.awk)
    
    export PGPASSWORD="$POSTGRES_PASSWORD"
-   awk -f envSubstitution.awk setSearchPath.sql
-   awk -f envSubstitution.awk setSearchPath.sql | psql  -h $POSTGRES_HOST -d $POSTGRES_DB -U $POSTGRES_USER
+   awk -f envSubstitution.awk setSearchPath.sql | tee | psql  -h $POSTGRES_HOST -d $POSTGRES_DB -U $POSTGRES_USER
    
    CREATED_USER_COUNT=$(awk -f envSubstitution.awk getValidUserCount.sql | psql -qtAX -h $POSTGRES_HOST -U $POSTGRES_USER)
    
