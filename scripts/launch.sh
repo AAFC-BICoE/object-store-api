9b21a0bb28d8a37d2443934407efac7273679479
Chalker, Tom <tom.chalker@canada.ca>
2020-03-21 17:55:37 -0400

    Replaced spring.datasource.url with constructed value

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -5,9 +5,14 @@ cd /app
 
 echo "checking for users"
 ./checkUsers.sh
-echo "url=$(echo '$spring.datasource.url' | awk  -f envSubstitution.awk)"
+
+# Need to build the spring.datasource.url as database and schema name may be set differently in the Env Var settings.
+SCHEMA=$(echo '$spring.liquibase.defaultSchema' | awk -f envSubstitution.awk)
+URL="jdbc:postgresql://$POSTGRES_HOST/$POSTGRES_DB?currentSchema=$SCHEMA"
+
 VERSION=$(cat ./pom.xml | grep -m 1 '<version>' | awk -F"[><]" '{print $3}')
-export VERSION
 echo "Version: '$VERSION'"
+
 echo "executing java"
-exec java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar $1-$VERSION.jar
+echo "java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap $ARGS -Dspring.datasource.url=$URL -jar $1-$VERSION.jar"
+exec java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap $ARGS -Dspring.datasource.url=$URL -jar $1-$VERSION.jar
