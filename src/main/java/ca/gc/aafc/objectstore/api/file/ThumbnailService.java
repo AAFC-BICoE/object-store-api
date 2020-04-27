dab7c48b51a55e94d48215f072caa4066ba29dc0
Jkeyuk <Jonkero2@gmail.com>
2020-04-27 10:15:19 -0400

    18625_thumbnail_derives_from_parent
    generateThumbnail no longer uses temp files

--- src/main/java/ca/gc/aafc/objectstore/api/file/ThumbnailService.java
+++ src/main/java/ca/gc/aafc/objectstore/api/file/ThumbnailService.java
@@ -2,14 +2,12 @@ package ca.gc.aafc.objectstore.api.file;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
 import javax.imageio.ImageIO;
 
-import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 
 import ca.gc.aafc.objectstore.api.dto.ObjectStoreMetadataDto;
@@ -27,19 +25,13 @@ public class ThumbnailService {
   public static final DcType THUMBNAIL_DC_TYPE = DcType.IMAGE;
 
   public InputStream generateThumbnail(InputStream sourceImageStream) throws IOException {
-    // Copy to a temp file; The source image must be a file for thumbnailator to
-    // generate the thumbnail.
-    File tempSourceFile = File.createTempFile(UUID.randomUUID().toString(), ".tmp");
-    FileUtils.copyToFile(sourceImageStream, tempSourceFile);
 
     try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
       // Create the thumbnail:
-      Thumbnails.of(tempSourceFile)
+      Thumbnails.of(sourceImageStream)
         .size(THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT)
         .outputFormat("jpg")
         .toOutputStream(os);
-
-      FileUtils.deleteQuietly(tempSourceFile);
 
       ByteArrayInputStream thumbnail = new ByteArrayInputStream(os.toByteArray());
       return thumbnail;
