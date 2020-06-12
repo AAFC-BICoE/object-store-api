3b4a361e1ded74dcfa5ca384b3989f198c4c4a09
tom <tom.chalker@canada.ca>
2020-06-12 13:56:41 -0400

    Parameterize POSTGRES_USER fix2

--- scripts/checkDatabaseReadiness.sh
+++ scripts/checkDatabaseReadiness.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-./tableExists.sh public DatabaseReadiness postgres
+./tableExists.sh public DatabaseReadiness $POSTGRES_DB
 if [ "$?" == 0 ]; then
    export PGPASSWORD="$POSTGRES_PASSWORD"
-   RECORD=$(envsubst <latestDbUpdate.sql | psql -qtAX  -h $POSTGRES_HOST -d postgres -U $POSTGRES_USER)
+   RECORD=$(envsubst <latestDbUpdate.sql | psql -qtAX  -h $POSTGRES_HOST -d $POSTGRES_DB -U $POSTGRES_USER)
    echo "status|process|id|lastModified = $RECORD"
    STATUS=$(echo $RECORD | awk -F"|" '{print $1}')
    if [ "$STATUS" == "3" ]; then
