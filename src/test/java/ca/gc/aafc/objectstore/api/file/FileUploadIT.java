39dd194106f40b8f4d53283f29dbb26711638909
Jkeyuk <Jonkero2@gmail.com>
2020-06-15 09:49:34 -0400

    19460_restrict_bucket_access_by_group
    Revert changes to previous test

--- src/test/java/ca/gc/aafc/objectstore/api/file/FileUploadIT.java
+++ src/test/java/ca/gc/aafc/objectstore/api/file/FileUploadIT.java
@@ -16,7 +16,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.util.NestedServletException;
 
 import ca.gc.aafc.objectstore.api.DinaAuthenticatedUserConfig;
-import io.crnk.core.exception.UnauthorizedException;
+import ca.gc.aafc.objectstore.api.TestConfiguration;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -40,19 +40,20 @@ public class FileUploadIT {
   }
 
   @Test
-  public void fileUpload_onInvalidBucket_UnauthorizedException() throws Exception {
+  public void fileUpload_onInvalidBucket_returnError() throws Exception {
 
     MockMultipartFile file = new MockMultipartFile("file", "testfile", MediaType.TEXT_PLAIN_VALUE,
         "Test Content".getBytes());
     
     try {
       webAppContextSetup(this.wac).build()
-          .perform(MockMvcRequestBuilders.multipart("/api/v1/file/a").file(file));
+      .perform(MockMvcRequestBuilders
+          .multipart("/api/v1/file/a" + TestConfiguration.ILLEGAL_BUCKET_CHAR + "b").file(file));
       fail("Expected NestedServletException");
     }
     // NestedServletException is a generic exception so we want to do the assertion on the cause
     catch (NestedServletException nsEx) {
-      assertEquals(UnauthorizedException.class, nsEx.getCause().getClass());
+      assertEquals(IllegalStateException.class, nsEx.getCause().getClass());
     }
      
   }
