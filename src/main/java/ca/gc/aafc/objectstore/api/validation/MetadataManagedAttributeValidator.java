2e78e2c9eabeb0f22ae6b429acf842852c4cd266
Belisle-Leclerc, Melanie <melanie.belisle-leclerc@canada.ca>
2020-07-21 16:02:47 -0400

    update MetadataMA validator to include type validation. fixed validation logic.

--- src/main/java/ca/gc/aafc/objectstore/api/validation/MetadataManagedAttributeValidator.java
+++ src/main/java/ca/gc/aafc/objectstore/api/validation/MetadataManagedAttributeValidator.java
@@ -1,29 +1,26 @@
 package ca.gc.aafc.objectstore.api.validation;
 
-import java.util.Arrays;
+import java.util.regex.Pattern;
+//import java.util.Arrays;
 import java.util.stream.Stream;
-
 import javax.annotation.Resource;
 import javax.inject.Inject;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import ca.gc.aafc.objectstore.api.entities.ManagedAttribute;
 import ca.gc.aafc.objectstore.api.entities.MetadataManagedAttribute;
+import ca.gc.aafc.objectstore.api.entities.ManagedAttribute.ManagedAttributeType;
 
 public class MetadataManagedAttributeValidator implements Validator {
  
     @Inject
     private final MessageSource messageSource;
 
-    }
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -34,20 +31,23 @@ public class MetadataManagedAttributeValidator implements Validator {
     public void validate(Object target, Errors errors) {
 
         MetadataManagedAttribute mma = (MetadataManagedAttribute) target;
-        
-        String[] acceptedValues = mma.getManagedAttribute().getAcceptedValues();
+        String assignedValue = mma.getAssignedValue();
+        ManagedAttribute ma = mma.getManagedAttribute();
+        String[] acceptedValues = ma.getAcceptedValues();
+        ManagedAttributeType maType = ma.getManagedAttributeType();
         
         if (acceptedValues.equals(null)) {
-            errors.rejectValue("assignedValue", "assignedValue.invalid"/*, arg2*/);
-        } else {
-            if(Stream.of(acceptedValues).map(x -> x.toUpperCase()).anyMatch(mma.getAssignedValue()::equals)){
-                errors.rejectValue("assignedValue", "assignedValue.invalid"/*, arg2*/);
+            if (maType == ManagedAttributeType.INTEGER) {
+                Pattern pattern = Pattern.compile("\\d+");
+                if (!pattern.matcher(assignedValue).matches()) {
+                    errors.rejectValue("assignedValue", "assignedValueType.invalid", new String[] {assignedValue}, "defaultMessage");
+                }   
             }
-            //if (!Arrays.stream(acceptedValues).anyMatch(mma.getAssignedValue()::equals)){
-            
-        }
-        
-        // check that assignedValue type is of type contained in ManagedAttributeType
+        } else {
+            if(!Stream.of(acceptedValues).map(x -> x.toUpperCase()).anyMatch(mma.getAssignedValue()::equals)){
+                errors.rejectValue("assignedValue", "assignedValue.invalid", new String[] {assignedValue}, null);
+            }
+        }        
 
     }
 }
