0df91cbfcac01163e6aa8d1065eff7592be12a0c
Jkeyuk <Jonkero2@gmail.com>
2020-07-22 09:50:52 -0400

    19843_Auditing_Service_layer
    - move getMetadata to service layer and make some methods static where applicable.
    - need to adjust getMetadata to return a count instead of crnk object

--- src/main/java/ca/gc/aafc/objectstore/api/respository/auditlog/AuditSnapshotRepository.java
+++ src/main/java/ca/gc/aafc/objectstore/api/respository/auditlog/AuditSnapshotRepository.java
@@ -10,9 +10,7 @@ import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.javers.core.Javers;
 import org.javers.core.metamodel.object.CdoSnapshot;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import ca.gc.aafc.dina.repository.NoLinkInformation;
@@ -23,7 +21,6 @@ import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.ReadOnlyResourceRepositoryBase;
 import io.crnk.core.resource.list.DefaultResourceList;
 import io.crnk.core.resource.list.ResourceList;
-import io.crnk.core.resource.meta.DefaultPagedMetaInformation;
 import io.crnk.core.resource.meta.PagedMetaInformation;
 import lombok.NonNull;
 
@@ -57,9 +54,9 @@ public class AuditSnapshotRepository extends ReadOnlyResourceRepositoryBase<Audi
     } 
 
     List<AuditSnapshotDto> dtos = AuditService.findAll(javers, type, id, authorFilter, limit, skip)
-      .stream().map(this::toDto).collect(Collectors.toList());
+      .stream().map(AuditSnapshotRepository::toDto).collect(Collectors.toList());
 
-    PagedMetaInformation meta = getMetadata(authorFilter, id, type);
+    PagedMetaInformation meta = AuditService.getMetadata(jdbcTemplate, authorFilter, id, type);
 
     return new DefaultResourceList<>(dtos, meta, new NoLinkInformation());
   }
@@ -71,7 +68,7 @@ public class AuditSnapshotRepository extends ReadOnlyResourceRepositoryBase<Audi
   }
 
   /** Converts Javers snapshot to our DTO format. */
-  private AuditSnapshotDto toDto(CdoSnapshot original) {
+  private static AuditSnapshotDto toDto(CdoSnapshot original) {
     // Get the snapshot state as a map:
     Map<String, Object> state = new HashMap<>();
     original.getState().forEachProperty((key, val) -> state.put(key, val));
@@ -91,35 +88,11 @@ public class AuditSnapshotRepository extends ReadOnlyResourceRepositoryBase<Audi
         .build();
   }
 
-  /** Get the meta information with the total resource count. */
-  private PagedMetaInformation getMetadata(String authorFilter, String id, String type) {
-    // Use sql to get the count because Javers does not provide a counting method:
-    SqlParameterSource parameters = new MapSqlParameterSource()
-      .addValue("author", authorFilter)
-      .addValue("id", "\"" + id + "\"") // Javers puts double-quotes around the id in the database.
-      .addValue("type", type);
-    
-    // Apply filters:
-    String baseSql = "select count(*) from jv_snapshot s join jv_commit c on s.commit_fk = c.commit_pk where 1=1 %s %s ;";
-    String sql = String.format(
-      baseSql,
-      StringUtils.isNotBlank(authorFilter) ? "and c.author = :author" : "",
-      StringUtils.isNotBlank(id) ? "and global_id_fk = (select global_id_pk from jv_global_id where local_id = :id and type_name = :type)" : ""
-    );
-
-    Long count = jdbcTemplate.queryForObject(sql, parameters, Long.class);
-
-    DefaultPagedMetaInformation meta = new DefaultPagedMetaInformation();
-    meta.setTotalResourceCount(count);
-
-    return meta;
-  }
-
   /**
    * Parses the id and type from the instanceFilter format.
    * e.g. metadata/42f1d751-f3ec-4c3b-9247-750f4e48ae04
    */
-  private String[] getIdAndType(@NonNull String instanceFilter) {
+  private static String[] getIdAndType(@NonNull String instanceFilter) {
     String[] split = instanceFilter.split("/");
     if (split.length != 2) {
       throw new IllegalArgumentException("Invalid ID must be formatted as {type}/{id}: " + instanceFilter);
@@ -132,7 +105,7 @@ public class AuditSnapshotRepository extends ReadOnlyResourceRepositoryBase<Audi
   /**
    * Converts Crnk's filters into a String/String Map.
    */
-  private Map<String, String> getFilterMap(QuerySpec qs) {
+  private static Map<String, String> getFilterMap(QuerySpec qs) {
     Map<String, String> map = new HashMap<>();
     qs.getFilters().forEach(
       it -> map.put(
--- src/main/java/ca/gc/aafc/objectstore/api/service/AuditService.java
+++ src/main/java/ca/gc/aafc/objectstore/api/service/AuditService.java
@@ -6,6 +6,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.javers.core.Javers;
 import org.javers.core.metamodel.object.CdoSnapshot;
 import org.javers.repository.jql.QueryBuilder;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import io.crnk.core.resource.meta.DefaultPagedMetaInformation;
+import io.crnk.core.resource.meta.PagedMetaInformation;
 
 public class AuditService {
 
@@ -26,6 +32,38 @@ public class AuditService {
     queryBuilder.skip(skip);
 
     return javers.findSnapshots(queryBuilder.build());
+  }
+
+  /**
+   * Get the meta information with the total resource count.
+   * 
+   * @param jdbcTemplate
+   * @param authorFilter
+   * @param id
+   * @param type
+   * @return
+   */
+  public static PagedMetaInformation getMetadata(NamedParameterJdbcTemplate jdbcTemplate, String authorFilter,
+      String id, String type) {
+    // Use sql to get the count because Javers does not provide a counting method:
+    SqlParameterSource parameters = new MapSqlParameterSource().addValue("author", authorFilter)
+        .addValue("id", "\"" + id + "\"") // Javers puts double-quotes around the id in the database.
+        .addValue("type", type);
+
+    // Apply filters:
+    String baseSql = "select count(*) from jv_snapshot s join jv_commit c on s.commit_fk = c.commit_pk where 1=1 %s %s ;";
+    String sql = 
+      String.format(
+        baseSql,
+        StringUtils.isNotBlank(authorFilter) ? "and c.author = :author" : "",
+        StringUtils.isNotBlank(id)
+            ? "and global_id_fk = (select global_id_pk from jv_global_id where local_id = :id and type_name = :type)"
+            : "");
+
+    Long count = jdbcTemplate.queryForObject(sql, parameters, Long.class);
+    DefaultPagedMetaInformation meta = new DefaultPagedMetaInformation();
+    meta.setTotalResourceCount(count);
+    return meta;
   }
 
 }
