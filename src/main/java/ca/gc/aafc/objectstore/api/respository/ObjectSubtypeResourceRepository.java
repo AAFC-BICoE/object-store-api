7fcf2890e38945e3b3be2d0aff55b3c445b1537f
Jkeyuk <Jonkero2@gmail.com>
2020-07-27 12:00:44 -0400

    19146_app_managed_subtype
    Update sub type repo to prevent changing a app managed subtype to Non managed

--- src/main/java/ca/gc/aafc/objectstore/api/respository/ObjectSubtypeResourceRepository.java
+++ src/main/java/ca/gc/aafc/objectstore/api/respository/ObjectSubtypeResourceRepository.java
@@ -12,12 +12,15 @@ import ca.gc.aafc.dina.repository.DinaRepository;
 import ca.gc.aafc.dina.service.DinaService;
 import ca.gc.aafc.objectstore.api.dto.ObjectSubtypeDto;
 import ca.gc.aafc.objectstore.api.entities.ObjectSubtype;
+import io.crnk.core.exception.ForbiddenException;
 import lombok.NonNull;
 
 @Repository
 @Transactional
 public class ObjectSubtypeResourceRepository
     extends DinaRepository<ObjectSubtypeDto, ObjectSubtype> {
+
+  private final DinaService<ObjectSubtype> dinaService;
 
   public ObjectSubtypeResourceRepository(
     @NonNull DinaService<ObjectSubtype> dinaService,
@@ -30,6 +33,16 @@ public class ObjectSubtypeResourceRepository
       ObjectSubtypeDto.class,
       ObjectSubtype.class,
       filterResolver);
+    this.dinaService = dinaService;
+  }
+
+  @Override
+  public <S extends ObjectSubtypeDto> S save(S resource) {
+    ObjectSubtype entity = dinaService.findOne(resource.getUuid(), ObjectSubtype.class);
+    if (entity.isAppManaged()) {
+      throw new ForbiddenException("This sub type is app managed and cannot be updated/deleted");
+    }
+    return super.save(resource);
   }
 
 }
