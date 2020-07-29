97efd32a0f30a240cf0378aeb3d166bfffe81a8e
Belisle-Leclerc, Melanie <melanie.belisle-leclerc@canada.ca>
2020-07-29 13:47:47 -0400

    Fix indent

--- src/main/java/ca/gc/aafc/objectstore/api/validation/MetadataManagedAttributeValidator.java
+++ src/main/java/ca/gc/aafc/objectstore/api/validation/MetadataManagedAttributeValidator.java
@@ -15,50 +15,50 @@ import ca.gc.aafc.objectstore.api.entities.ManagedAttribute.ManagedAttributeType
 
 public class MetadataManagedAttributeValidator implements Validator {
 
-	private final MessageSource messageSource;
-	private static final Pattern INTEGER_PATTERN = Pattern.compile("\\d+");
+  private final MessageSource messageSource;
+  private static final Pattern INTEGER_PATTERN = Pattern.compile("\\d+");
 
-	public MetadataManagedAttributeValidator(MessageSource messageSource) {
-		this.messageSource = messageSource;
-	}
+  public MetadataManagedAttributeValidator(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
 
-	@Override
-	public boolean supports(Class<?> clazz) {
-		return MetadataManagedAttribute.class.isAssignableFrom(clazz);
-	}
+  @Override
+  public boolean supports(Class<?> clazz) {
+    return MetadataManagedAttribute.class.isAssignableFrom(clazz);
+  }
 
-	@Override
-	public void validate(Object target, Errors errors) {
-		MetadataManagedAttribute mma = (MetadataManagedAttribute) target;
-		String assignedValue = mma.getAssignedValue();
-		ManagedAttribute ma = mma.getManagedAttribute();
-		ArrayList<String> acceptedValues = getAcceptedValuesList(ma.getAcceptedValues());
-		ManagedAttributeType maType = ma.getManagedAttributeType();
-		boolean assignedValueIsValid = true;
+  @Override
+  public void validate(Object target, Errors errors) {
+    MetadataManagedAttribute mma = (MetadataManagedAttribute) target;
+    String assignedValue = mma.getAssignedValue();
+    ManagedAttribute ma = mma.getManagedAttribute();
+    ArrayList<String> acceptedValues = getAcceptedValuesList(ma.getAcceptedValues());
+    ManagedAttributeType maType = ma.getManagedAttributeType();
+    boolean assignedValueIsValid = true;
 
-		if (acceptedValues.isEmpty()) {
-			if (maType == ManagedAttributeType.INTEGER && !INTEGER_PATTERN.matcher(assignedValue).matches()) {
-				assignedValueIsValid = false;
-			}
-		} else {
-			if (!acceptedValues.contains(assignedValue.toUpperCase())) {
-				assignedValueIsValid = false;
-			}
-		}
-		if (!assignedValueIsValid) {
-			String errorMessage = messageSource.getMessage("assignedValue.invalid", new String[] { assignedValue },
-					LocaleContextHolder.getLocale());
-			errors.rejectValue("assignedValue", "assignedValue.invalid", new String[] { assignedValue }, errorMessage);
-		}
-	}
-	
-	private ArrayList<String> getAcceptedValuesList(String[] acceptedValues) {
-		if (acceptedValues != null) {
-			ArrayList<String> result = new ArrayList<String>(Arrays.asList(acceptedValues));
-			result.replaceAll(x -> x.toUpperCase());
-			return result;
-		} else {
-			return new ArrayList<String>();
-		}
-	}
+    if (acceptedValues.isEmpty()) {
+      if (maType == ManagedAttributeType.INTEGER && !INTEGER_PATTERN.matcher(assignedValue).matches()) {
+        assignedValueIsValid = false;
+      }
+    } else {
+      if (!acceptedValues.contains(assignedValue.toUpperCase())) {
+        assignedValueIsValid = false;
+      }
+    }
+    if (!assignedValueIsValid) {
+      String errorMessage = messageSource.getMessage("assignedValue.invalid", new String[] { assignedValue },
+          LocaleContextHolder.getLocale());
+      errors.rejectValue("assignedValue", "assignedValue.invalid", new String[] { assignedValue }, errorMessage);
+    }
+  }
+  
+  private ArrayList<String> getAcceptedValuesList(String[] acceptedValues) {
+    if (acceptedValues != null) {
+      ArrayList<String> result = new ArrayList<String>(Arrays.asList(acceptedValues));
+      result.replaceAll(x -> x.toUpperCase());
+      return result;
+    } else {
+      return new ArrayList<String>();
+    }
+  }
 }
