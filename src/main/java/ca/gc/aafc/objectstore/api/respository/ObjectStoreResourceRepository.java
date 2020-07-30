20bbac362db82230bb96ded2eed7e4d45479b6f7
Jkeyuk <Jonkero2@gmail.com>
2020-07-30 10:14:51 -0400

    19649_metadata_uses_dina_layers
    - Remove dina base stuff
    - Update Pom for next compatible version

--- src/main/java/ca/gc/aafc/objectstore/api/respository/ObjectStoreResourceRepository.java
+++ src/main/java/ca/gc/aafc/objectstore/api/respository/ObjectStoreResourceRepository.java
@@ -18,6 +18,8 @@ import org.springframework.stereotype.Repository;
 
 import ca.gc.aafc.dina.entity.SoftDeletable;
 import ca.gc.aafc.dina.filter.DinaFilterResolver;
+import ca.gc.aafc.dina.mapper.DinaMapper;
+import ca.gc.aafc.dina.repository.DinaRepository;
 import ca.gc.aafc.dina.repository.GoneException;
 import ca.gc.aafc.dina.service.DinaService;
 import ca.gc.aafc.objectstore.api.dto.ObjectStoreMetadataDto;
@@ -60,7 +62,7 @@ public class ObjectStoreResourceRepository
     super(
       dinaService,
       Optional.empty(),
-      new DinaMapper<ObjectStoreMetadataDto, ObjectStoreMetadata>(ObjectStoreMetadataDto.class),
+      new DinaMapper<>(ObjectStoreMetadataDto.class),
       ObjectStoreMetadataDto.class,
       ObjectStoreMetadata.class,
       filterResolver);
