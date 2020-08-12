0e392b627cb4445f8007f18f4796d4c7a115ddb7
tom <tom.chalker@canada.ca>
2020-08-12 13:06:53 -0400

    Separated root database from application database

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,8 +4,12 @@ cd /app
 echo "Appending env var translated from dot to underscore"
 eval $(awk -f ./addEnvForDotWithUnderscore.awk)
 
+if [ -z "${spring_datasource_database}" ]; then
+    export spring_datasource_database=$POSTGRES_DB
+fi
+
 echo "Postgress database $POSTGRES_DB on $POSTGRES_HOST:5432 with root user $POSTGRES_USER"
-echo "application = $APPLICATION, database = $POSTGRES_DB, schema = $spring_liquibase_defaultSchema"
+echo "application = $APPLICATION, database = $spring_datasource_database, schema = $spring_liquibase_defaultSchema"
 
 ./waitForDatabase.sh
 if [ 0 != $? ]; then
@@ -16,7 +20,7 @@ fi
 ./checkUsers.sh
 
 # Need to build the spring.datasource.url as database and schema name may be set differently in the Env Var settings.
-URL="jdbc:postgresql://$POSTGRES_HOST/$POSTGRES_DB?currentSchema=$spring_liquibase_defaultSchema"
+URL="jdbc:postgresql://$POSTGRES_HOST/$spring_datasource_database?currentSchema=$spring_liquibase_defaultSchema"
 
 VERSION=$(cat ./pom.xml | grep -m 1 '<version>' | awk -F"[><]" '{print $3}')
 echo "Version: '$VERSION'"
