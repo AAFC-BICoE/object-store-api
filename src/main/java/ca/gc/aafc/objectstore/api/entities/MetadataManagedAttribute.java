355f5e9b1c662ac1e2e4cab2ec81158186dfccd9
Jkeyuk <Jonkero2@gmail.com>
2020-08-24 07:58:22 -0400

    20096 implement dina entity fields
    Checkstyle: revert use of * in imports

--- src/main/java/ca/gc/aafc/objectstore/api/entities/MetadataManagedAttribute.java
+++ src/main/java/ca/gc/aafc/objectstore/api/entities/MetadataManagedAttribute.java
@@ -8,7 +8,16 @@ import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.hibernate.annotations.NaturalId;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.PreUpdate;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
