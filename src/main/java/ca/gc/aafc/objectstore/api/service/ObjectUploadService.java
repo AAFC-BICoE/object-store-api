aa45a31069fcb15022a03bfe06f37a8d034ed468
xuemeigan <xuemei.gan@canada.ca>
2020-08-25 16:05:02 -0400

    Support 20132 Implement ObjectUpload Service - Add implementation to prevent from crud operation

--- src/main/java/ca/gc/aafc/objectstore/api/service/ObjectUploadService.java
+++ src/main/java/ca/gc/aafc/objectstore/api/service/ObjectUploadService.java
@@ -4,12 +4,39 @@ import ca.gc.aafc.dina.jpa.BaseDAO;
 import ca.gc.aafc.dina.service.DinaService;
 import ca.gc.aafc.objectstore.api.entities.ObjectUpload;
 import lombok.NonNull;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ObjectUploadService extends DinaService<ObjectUpload> {
 
-  public ObjectUploadService(@NonNull BaseDAO baseDAO) {
+  private final MessageSource messageSource;
+
+  public ObjectUploadService(@NonNull BaseDAO baseDAO, MessageSource messageSource) {
     super(baseDAO);
+    this.messageSource = messageSource;
   }
+
+  @Override
+  protected void preCreate(ObjectUpload entity) {
+    throw new AccessDeniedException(getMessage("error.objectUpload.create_unsupported"));
+  }
+
+  @Override
+  protected void preDelete(ObjectUpload entity) {
+    throw new AccessDeniedException(getMessage("error.objectUpload.delete_unsupported"));
+  }
+
+  @Override
+  protected void preUpdate(ObjectUpload entity) {
+    throw new AccessDeniedException(getMessage("error.objectUpload.update_unsupported"));
+  }
+
+  private String getMessage(String key) {
+    return messageSource.getMessage(key, null, LocaleContextHolder.getLocale());
+  }
+
 }
