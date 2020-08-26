2bba57dfd931f0e3e1a9d23502116d8464040876
xuemeigan <xuemei.gan@canada.ca>
2020-08-26 11:54:14 -0400

    Support 19960 Expose uploaded object - Add overridden findAll method in ObjectUploadResourceRepository

--- src/main/java/ca/gc/aafc/objectstore/api/respository/ObjectUploadResourceRepository.java
+++ src/main/java/ca/gc/aafc/objectstore/api/respository/ObjectUploadResourceRepository.java
@@ -1,28 +1,91 @@
 package ca.gc.aafc.objectstore.api.respository;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.springframework.stereotype.Repository;
 
+import ca.gc.aafc.dina.filter.DinaFilterResolver;
+import ca.gc.aafc.dina.mapper.DinaMapper;
+import ca.gc.aafc.dina.repository.NoLinkInformation;
+import ca.gc.aafc.dina.repository.SelectionHandler;
 import ca.gc.aafc.objectstore.api.dto.ObjectUploadDto;
+import ca.gc.aafc.objectstore.api.entities.ObjectUpload;
 import ca.gc.aafc.objectstore.api.service.ObjectUploadService;
+import io.crnk.core.engine.registry.ResourceRegistry;
 import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.ReadOnlyResourceRepositoryBase;
+import io.crnk.core.resource.list.DefaultResourceList;
 import io.crnk.core.resource.list.ResourceList;
+import io.crnk.core.resource.meta.DefaultPagedMetaInformation;
+import lombok.Getter;
+import lombok.Setter;
 
 @Repository
-public class ObjectUploadResourceRepository extends ReadOnlyResourceRepositoryBase<ObjectUploadDto, UUID> {
-    private final ObjectUploadService service;
+public class ObjectUploadResourceRepository
+    extends ReadOnlyResourceRepositoryBase<ObjectUploadDto, UUID> {
+  private final ObjectUploadService service;
+  private final DinaFilterResolver filterResolver;
+  private static final NoLinkInformation NO_LINK_INFORMATION = new NoLinkInformation();
+  private DinaMapper<ObjectUploadDto, ObjectUpload> dinaMapper;
 
-    protected ObjectUploadResourceRepository(ObjectUploadService service) {
-        super(ObjectUploadDto.class);
-        this.service = service;
-    }
+  private static final int DEFAULT_OFFSET = 0;
+  private static final int DEFAULT_LIMIT = 100;
 
-    @Override
-    public ResourceList<ObjectUploadDto> findAll(QuerySpec querySpec) {
-        //this.service.findAll(entityClass, where, orderBy, startIndex, maxResult)
-        return null;
-    }
-    
+  @Getter
+  @Setter(onMethod_ = @Override)
+  private ResourceRegistry resourceRegistry;
+
+  protected ObjectUploadResourceRepository(ObjectUploadService service,
+      DinaFilterResolver filterResolver) {
+    super(ObjectUploadDto.class);
+    this.service = service;
+    this.filterResolver = filterResolver;
+    this.dinaMapper = new DinaMapper<>(ObjectUploadDto.class);
+  }
+
+  @Override
+  public ResourceList<ObjectUploadDto> findAll(QuerySpec querySpec) {
+
+    String idName = SelectionHandler.getIdAttribute(ObjectUploadDto.class, resourceRegistry);
+
+    List<ObjectUpload> returnedEntities = service.findAll(ObjectUpload.class,
+      (cb, root) -> filterResolver.buildPredicates(querySpec, cb, root, null, idName),
+      (cb, root) -> DinaFilterResolver.getOrders(querySpec, cb, root),
+        Optional.ofNullable(querySpec.getOffset()).orElse(Long.valueOf(DEFAULT_OFFSET)).intValue(),
+        Optional.ofNullable(querySpec.getLimit()).orElse(Long.valueOf(DEFAULT_LIMIT)).intValue());
+
+    Set<String> includedRelations = querySpec.getIncludedRelations().stream()
+        .map(ir -> ir.getAttributePath().get(0)).collect(Collectors.toSet());
+
+    Map<Class<?>, Set<String>> entityFieldsPerClass = new HashMap<Class<?>, Set<String>>();
+
+    List<Field> attributeFields = FieldUtils.getAllFieldsList(ObjectUpload.class).stream()
+        .collect(Collectors.toList());
+
+    Set<String> fieldsToInclude = attributeFields.stream().map(af -> af.getName())
+        .collect(Collectors.toSet());
+
+    entityFieldsPerClass.put(ObjectUpload.class, fieldsToInclude);
+
+    List<ObjectUploadDto> dtos = returnedEntities.stream()
+        .map(e -> dinaMapper.toDto(e, entityFieldsPerClass, includedRelations))
+        .collect(Collectors.toList());
+
+    Long resourceCount = service.getResourceCount(ObjectUpload.class,
+      (cb, root) -> filterResolver.buildPredicates(querySpec, cb, root, null, idName));
+
+    DefaultPagedMetaInformation metaInformation = new DefaultPagedMetaInformation();
+    metaInformation.setTotalResourceCount(resourceCount);
+    return new DefaultResourceList<>(dtos, metaInformation, NO_LINK_INFORMATION);
+
+  }
+
 }
