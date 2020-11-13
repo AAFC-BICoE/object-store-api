7368213aecb51e2f7e4994712e7afebda14e1fbf
Christian Gendreau <cgendreau@users.noreply.github.com>
2020-11-13 10:54:52 -0500

    20902 exif data extraction (#171)
    * 20902 exif data extraction
    * add parse date with test
    * Simplified date extraction method and add test for non-supported file
    
    Co-authored-by: @Jkeyuk

---
+++ src/test/java/ca/gc/aafc/objectstore/api/exif/ExifParserTest.java
@@ -0,0 +1,36 @@
+package ca.gc.aafc.objectstore.api.exif;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileInputStream;
+import java.util.Optional;
+
+class ExifParserTest {
+
+  private static final ClassPathResource TEST_PIC = new ClassPathResource("testPic.jpg");
+  private static final ClassPathResource TEST_FILE = new ClassPathResource("testfile.txt");
+
+  @SneakyThrows
+  @Test
+  void extractDateTaken_OnEXIFAvailable_EXIFExtracted() {
+    try (FileInputStream fis = new FileInputStream(TEST_PIC.getFile())) {
+      Optional<String> date = ExifParser.extractDateTaken(fis);
+      Assertions.assertTrue(date.isPresent());
+      Assertions.assertEquals("2020:11:13 10:03:17", date.get());
+    }
+
+  }
+
+  @SneakyThrows
+  @Test
+  void extractDateTaken_NoEXIFAvailable_NothingReturned() {
+    try (FileInputStream fis = new FileInputStream(TEST_FILE.getFile())) {
+      Optional<String> date = ExifParser.extractDateTaken(fis);
+      Assertions.assertTrue(date.isEmpty());
+    }
+  }
+
+}
