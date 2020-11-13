2e98ef7922ae832c5146ffaa010f1ceb6928f3a3
Christian Gendreau <christiangendreau@gmail.com>
2020-11-13 10:33:43 -0500

    Support #20902 Try to extract Date taken from image exif Simplified date extraction method and add test for non-supported file

--- src/main/java/ca/gc/aafc/objectstore/api/exif/ExifParser.java
+++ src/main/java/ca/gc/aafc/objectstore/api/exif/ExifParser.java
@@ -1,34 +1,55 @@
 package ca.gc.aafc.objectstore.api.exif;
 
 import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
-import lombok.SneakyThrows;
+import lombok.NonNull;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Optional;
 
+/**
+ * Parse EXIF data from an InputStream and extract relevant data.
+ */
+@Log4j2
 public final class ExifParser {
+
+  private static final List<Integer> DATE_TAKEN_POSSIBLE_TAGS = List.of(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL,
+    ExifSubIFDDirectory.TAG_DATETIME, ExifSubIFDDirectory.TAG_DATETIME_DIGITIZED);
 
   private ExifParser() {
   }
 
-  @SneakyThrows
-  public static Metadata parse(InputStream inputStream) {
-    return ImageMetadataReader.readMetadata(inputStream, 10 * 1024L);
-  }
+  /**
+   * Try to extract the date from the EXIF in provided {@link InputStream}.
+   * If the format is unsupported {@link Optional#empty()} is returned
+   * @param inputStream
+   * @return date as string or @link Optional#empty()} if can't be extracted.
+   * @throws IOException
+   */
+  public static Optional<String> extractDateTaken(@NonNull InputStream inputStream) throws IOException {
+    Metadata metadata;
+    try {
+      metadata = ImageMetadataReader.readMetadata(inputStream);
+    } catch (ImageProcessingException e) {
+      log.debug(e);
+      return Optional.empty();
+    }
 
-  public static Optional<String> parseDate(Metadata metadata) {
     ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-    if (directory != null) {
-      String description = directory.getDescription(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-      if (description == null) {
-        description = directory.getDescription(ExifSubIFDDirectory.TAG_DATETIME);
+    if(directory == null) {
+      return Optional.empty();
+    }
+
+    for(Integer tag: DATE_TAKEN_POSSIBLE_TAGS) {
+      if(StringUtils.isNotBlank(directory.getDescription(tag))) {
+        return Optional.of(directory.getDescription(tag));
       }
-      if (description == null) {
-        description = directory.getDescription(ExifSubIFDDirectory.TAG_DATETIME_DIGITIZED);
-      }
-      return Optional.ofNullable(description);
     }
     return Optional.empty();
   }
--- src/test/java/ca/gc/aafc/objectstore/api/exif/ExifParserTest.java
+++ src/test/java/ca/gc/aafc/objectstore/api/exif/ExifParserTest.java
@@ -10,14 +10,22 @@ import java.util.Optional;
 
 class ExifParserTest {
 
-  public static final ClassPathResource TEST_PIC = new ClassPathResource("testPic.JPG");
+  private static final ClassPathResource TEST_PIC = new ClassPathResource("testPic.jpg");
+  private static final ClassPathResource TEST_FILE = new ClassPathResource("testfile.txt");
 
   @SneakyThrows
   @Test
-  void parseDate() {
-    Optional<String> date = ExifParser.parseDate(ExifParser.parse(new FileInputStream(TEST_PIC.getFile())));
+  void extractDateTaken_OnEXIFAvailable_EXIFExtracted() {
+    Optional<String> date = ExifParser.extractDateTaken(new FileInputStream(TEST_PIC.getFile()));
     Assertions.assertTrue(date.isPresent());
     Assertions.assertEquals("2020:11:13 10:03:17", date.get());
+  }
+
+  @SneakyThrows
+  @Test
+  void extractDateTaken_NoEXIFAvailable_NothingReturned() {
+    Optional<String> date = ExifParser.extractDateTaken(new FileInputStream(TEST_FILE.getFile()));
+    Assertions.assertTrue(date.isEmpty());
   }
 
 }
