222d8ac2bea151d5c8499f531a46c426c1a934da
Jkeyuk <jonkero2@gmail.com>
2020-12-21 13:45:55 -0500

    21243 add dctype to object upload
    adjust config to be dynamic list

--- src/main/java/ca/gc/aafc/objectstore/api/DefaultValueConfiguration.java
+++ src/main/java/ca/gc/aafc/objectstore/api/DefaultValueConfiguration.java
@@ -2,18 +2,26 @@ package ca.gc.aafc.objectstore.api;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+import java.util.List;
+
 @ConfigurationProperties(prefix = "default-values")
-@ConstructorBinding
-@RequiredArgsConstructor
 @Getter
+@Setter
 public class DefaultValueConfiguration {
 
-  private final String defaultLicenceURL;
-  private final String defaultCopyright;
-  private final String defaultCopyrightOwner;
-  private final String defaultUsageTerms;
+  private List<DefaultValue> values;
+
+  @ConstructorBinding
+  @RequiredArgsConstructor
+  @Getter
+  public static class DefaultValue {
+    private final String type;
+    private final String attribute;
+    private final String value;
+  }
 
 }
--- src/main/java/ca/gc/aafc/objectstore/api/dto/DefaultValuesDto.java
+++ src/main/java/ca/gc/aafc/objectstore/api/dto/DefaultValuesDto.java
@@ -2,15 +2,16 @@ package ca.gc.aafc.objectstore.api.dto;
 
 import io.crnk.core.resource.annotations.JsonApiId;
 import io.crnk.core.resource.annotations.JsonApiResource;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @JsonApiResource(type = "default-values")
 public class DefaultValuesDto {
   @JsonApiId
   private Integer id;
-  private String defaultLicenceURL;
-  private String defaultCopyright;
-  private String defaultCopyrightOwner;
-  private String defaultUsageTerms;
+  private final String type;
+  private final String attribute;
+  private final String value;
 }
