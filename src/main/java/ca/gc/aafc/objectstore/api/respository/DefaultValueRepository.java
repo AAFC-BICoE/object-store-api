2dbd6797d26dc107ea62ceafce6515bbaf389da4
Jkeyuk <jonkero2@gmail.com>
2020-12-21 13:52:25 -0500

    21244 default values endpoint
    update default value repo for new structure

--- src/main/java/ca/gc/aafc/objectstore/api/respository/DefaultValueRepository.java
+++ src/main/java/ca/gc/aafc/objectstore/api/respository/DefaultValueRepository.java
@@ -2,35 +2,49 @@ package ca.gc.aafc.objectstore.api.respository;
 
 import ca.gc.aafc.objectstore.api.DefaultValueConfiguration;
 import ca.gc.aafc.objectstore.api.dto.DefaultValuesDto;
+import io.crnk.core.exception.ResourceNotFoundException;
 import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.ReadOnlyResourceRepositoryBase;
 import io.crnk.core.resource.list.ResourceList;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class DefaultValueRepository extends ReadOnlyResourceRepositoryBase<DefaultValuesDto, String> {
 
-  private final DefaultValuesDto defaultResource;
+  private final List<DefaultValuesDto> defaultResource;
 
   protected DefaultValueRepository(DefaultValueConfiguration configuration) {
     super(DefaultValuesDto.class);
-    defaultResource = new DefaultValuesDto();
-    defaultResource.setId(1);
-    defaultResource.setDefaultCopyright(configuration.getDefaultCopyright());
-    defaultResource.setDefaultLicenceURL(configuration.getDefaultLicenceURL());
-    defaultResource.setDefaultCopyrightOwner(configuration.getDefaultCopyrightOwner());
-    defaultResource.setDefaultUsageTerms(configuration.getDefaultUsageTerms());
+    List<DefaultValuesDto> list = new ArrayList<>();
+    if (CollectionUtils.isNotEmpty(configuration.getValues())) {
+      int index = 1;
+      for (DefaultValueConfiguration.DefaultValue defaultValue : configuration.getValues()) {
+        DefaultValuesDto build = DefaultValuesDto.builder()
+          .id(index++)
+          .value(defaultValue.getValue())
+          .type(defaultValue.getType())
+          .attribute(defaultValue.getAttribute())
+          .build();
+        list.add(build);
+      }
+    }
+    defaultResource = list;
   }
 
   @Override
   public ResourceList<DefaultValuesDto> findAll(QuerySpec querySpec) {
-    return querySpec.apply(List.of(defaultResource));
+    return querySpec.apply(defaultResource);
   }
 
   @Override
   public DefaultValuesDto findOne(String id, QuerySpec querySpec) {
-    return defaultResource;
+    return defaultResource.stream()
+      .filter(defaultValuesDto -> defaultValuesDto.getId().equals(Integer.valueOf(id))).findAny()
+      .orElseThrow(() -> new ResourceNotFoundException("Could not find a default value with id " + id));
+
   }
 }
