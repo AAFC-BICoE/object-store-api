bd30afd31f225688d93c096e77e18494338c3612
Jkeyuk <jonkero2@gmail.com>
2020-12-22 10:20:36 -0500

    21244 default values endpoint
    set default values dynamically with reflection

--- src/main/java/ca/gc/aafc/objectstore/api/service/ObjectStoreMetadataDefaultValueSetterService.java
+++ src/main/java/ca/gc/aafc/objectstore/api/service/ObjectStoreMetadataDefaultValueSetterService.java
@@ -1,24 +1,24 @@
 package ca.gc.aafc.objectstore.api.service;
 
+import ca.gc.aafc.objectstore.api.DefaultValueConfiguration;
+import ca.gc.aafc.objectstore.api.MediaTypeToDcTypeConfiguration;
+import ca.gc.aafc.objectstore.api.dto.ObjectStoreMetadataDto;
+import ca.gc.aafc.objectstore.api.entities.DcType;
+import ca.gc.aafc.objectstore.api.entities.ObjectStoreMetadata;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.regex.Pattern;
-
-import javax.inject.Inject;
-
-import ca.gc.aafc.objectstore.api.dto.ObjectStoreMetadataDto;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Service;
-
-import ca.gc.aafc.objectstore.api.MediaTypeToDcTypeConfiguration;
-import ca.gc.aafc.objectstore.api.DefaultValueConfiguration;
-import ca.gc.aafc.objectstore.api.entities.DcType;
-import ca.gc.aafc.objectstore.api.entities.ObjectStoreMetadata;
-import lombok.NonNull;
 
 /**
  * Service that contains logic around setting default values for various fields on {@link ObjectStoreMetadata}.
@@ -28,11 +28,6 @@ public class ObjectStoreMetadataDefaultValueSetterService {
 
   private final Map<String, String> defaultMetaValues;
   private final Set<Entry<DcType, LinkedList<Pattern>>> dcFormatToDcType;
-
-  private static final String KEY_LICENCE_URL = "xmpRightsWebStatement";
-  private static final String KEY_COPYRIGHT = "dcRights";
-  private static final String KEY_COPYRIGHT_OWNER = "xmpRightsOwner";
-  private static final String KEY_USAGE_TERMS = "xmpRightsUsageTerms";
 
   @Inject
   public ObjectStoreMetadataDefaultValueSetterService(
@@ -47,10 +42,10 @@ public class ObjectStoreMetadataDefaultValueSetterService {
     HashMap<String, String> map = new HashMap<>();
     if (config != null && CollectionUtils.isNotEmpty(config.getValues())) {
       config.getValues().stream()
-        .filter(v -> StringUtils.equalsIgnoreCase(v.getType(),ObjectStoreMetadataDto.TYPENAME))
+        .filter(v -> StringUtils.equalsIgnoreCase(v.getType(), ObjectStoreMetadataDto.TYPENAME))
         .forEach(value -> map.putIfAbsent(value.getAttribute(), value.getValue()));
     }
-    return map;
+    return Map.copyOf(map);
   }
 
   /**
@@ -60,27 +55,16 @@ public class ObjectStoreMetadataDefaultValueSetterService {
    * @param objectMetadata meta data to set
    * @return the meta data
    */
+  @SneakyThrows
   public ObjectStoreMetadata assignDefaultValues(@NonNull ObjectStoreMetadata objectMetadata) {
     if (objectMetadata.getDcType() == null) {
       objectMetadata.setDcType(dcTypeFromDcFormat(objectMetadata.getDcFormat()));
     }
-
-    if (StringUtils.isBlank(objectMetadata.getXmpRightsWebStatement())) {
-      objectMetadata.setXmpRightsWebStatement(defaultMetaValues.get(KEY_LICENCE_URL));
+    for (Entry<String, String> entry : defaultMetaValues.entrySet()) {
+      if (StringUtils.isBlank(BeanUtils.getProperty(objectMetadata, entry.getKey()))) {
+        BeanUtils.setProperty(objectMetadata, entry.getKey(), entry.getValue());
+      }
     }
-
-    if (StringUtils.isBlank(objectMetadata.getDcRights())) {
-      objectMetadata.setDcRights(defaultMetaValues.get(KEY_COPYRIGHT));
-    }
-
-    if (StringUtils.isBlank(objectMetadata.getXmpRightsOwner())) {
-      objectMetadata.setXmpRightsOwner(defaultMetaValues.get(KEY_COPYRIGHT_OWNER));
-    }
-
-    if (StringUtils.isBlank(objectMetadata.getXmpRightsUsageTerms())) {
-      objectMetadata.setXmpRightsUsageTerms(defaultMetaValues.get(KEY_USAGE_TERMS));
-    }
-
     return objectMetadata;
   }
 
