7120baf5fde605a4d00c2b156f145e83b748ede1
xuemeigan <xuemei.gan@canada.ca>
2021-01-08 08:19:16 -0500

    Support 21330 Try setting Cache headers for semi-static endpoints - Update test and fix checkstyle

--- src/test/java/ca/gc/aafc/objectstore/api/MainConfigurationTest.java
+++ src/test/java/ca/gc/aafc/objectstore/api/MainConfigurationTest.java
@@ -4,8 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,6 +14,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MainConfigurationTest extends BaseIntegrationTest{
+
+  private static final String CACHE_CONTROLLED_PATH = "/api/v1/license" ;
 
   @Autowired
   private WebApplicationContext wac;
@@ -29,7 +29,7 @@ public class MainConfigurationTest extends BaseIntegrationTest{
 
   @Test
   public void whenAccessLicence_cacheControlAddedToResponseHeader() throws Exception {
-    this.mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/license"))
+    this.mockMvc.perform(MockMvcRequestBuilders.get(CACHE_CONTROLLED_PATH))
       .andDo(MockMvcResultHandlers.print())
       .andExpect(MockMvcResultMatchers.header()
       .string("Cache-Control","max-age=86400, must-revalidate, no-transform"));
