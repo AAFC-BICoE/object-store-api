3e0df914a0150353a6f105621b117b778f3026d9
Jkeyuk <jonkero2@gmail.com>
2021-02-05 11:16:03 -0500

    21598 delete managed attribute in use
    override delete to use service to call children

--- src/main/java/ca/gc/aafc/objectstore/api/respository/ManagedAttributeResourceRepository.java
+++ src/main/java/ca/gc/aafc/objectstore/api/respository/ManagedAttributeResourceRepository.java
@@ -1,5 +1,6 @@
 package ca.gc.aafc.objectstore.api.respository;
 
+import ca.gc.aafc.dina.entity.DinaEntity;
 import ca.gc.aafc.dina.filter.DinaFilterResolver;
 import ca.gc.aafc.dina.mapper.DinaMapper;
 import ca.gc.aafc.dina.repository.DinaRepository;
@@ -7,11 +8,16 @@ import ca.gc.aafc.dina.security.DinaAuthenticatedUser;
 import ca.gc.aafc.dina.service.DinaService;
 import ca.gc.aafc.objectstore.api.dto.ManagedAttributeDto;
 import ca.gc.aafc.objectstore.api.entities.ManagedAttribute;
+import ca.gc.aafc.objectstore.api.entities.MetadataManagedAttribute;
 import ca.gc.aafc.objectstore.api.service.ManagedAttributeAuthorizationService;
+import io.crnk.core.exception.ResourceNotFoundException;
 import lombok.NonNull;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpClientErrorException;
 
+import javax.persistence.criteria.Predicate;
+import java.io.Serializable;
 import java.util.Optional;
 
 @Repository
@@ -19,6 +25,7 @@ public class ManagedAttributeResourceRepository
   extends DinaRepository<ManagedAttributeDto, ManagedAttribute> {
 
   private final Optional<DinaAuthenticatedUser> authenticatedUser;
+  private final DinaService<ManagedAttribute> dinaService;
 
   public ManagedAttributeResourceRepository(
     @NonNull DinaService<ManagedAttribute> dinaService,
@@ -36,6 +43,7 @@ public class ManagedAttributeResourceRepository
       ManagedAttribute.class, filterResolver, null,
       props);
     this.authenticatedUser = authenticatedUser;
+    this.dinaService = dinaService;
   }
 
   @Override
@@ -44,4 +52,20 @@ public class ManagedAttributeResourceRepository
     return super.create(resource);
   }
 
+  @Override
+  public void delete(Serializable id) {
+    ManagedAttribute entity = this.dinaService.findOne(id, ManagedAttribute.class);
+    if (entity == null) {
+      throw new ResourceNotFoundException("ManagedAttribute with id " + id + " not found.");
+    }
+
+    int size = dinaService.findAll(
+      MetadataManagedAttribute.class,
+      (cb, root) -> new Predicate[]{cb.equal(root.get("managedAttribute"), entity)}, null, 0, 1000).size();
+    if (size > 0) {
+
+    }
+
+    super.delete(id);
+  }
 }
