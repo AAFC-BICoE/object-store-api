b6e5c37e896ad4cef878b3ffb74db7b061875bc9
Jkeyuk <jonkero2@gmail.com>
2021-03-16 11:39:59 -0400

    22039 derivative entity
    update test case

--- src/test/java/ca/gc/aafc/objectstore/api/crud/DerivativeCRUDIT.java
+++ src/test/java/ca/gc/aafc/objectstore/api/crud/DerivativeCRUDIT.java
@@ -4,30 +4,48 @@ import ca.gc.aafc.objectstore.api.entities.DcType;
 import ca.gc.aafc.objectstore.api.entities.Derivative;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.UUID;
 
 public class DerivativeCRUDIT extends BaseEntityCRUDIT {
 
-  @Override
-  public void testSave() {
-    Derivative derivative = Derivative.builder()
+  private Derivative derivative;
+
+  @BeforeEach
+  void setUp() {
+    derivative = Derivative.builder()
       .uuid(UUID.randomUUID())
       .fileIdentifier(UUID.randomUUID())
       .fileExtension(".jpg")
       .bucket("mybucket")
       .acHashValue("abc")
+      .acHashFunction("abcFunction")
       .dcType(DcType.IMAGE)
       .createdBy(RandomStringUtils.random(4))
       .build();
     service.save(derivative);
+  }
+
+  @Override
+  public void testSave() {
     Assertions.assertNotNull(derivative.getCreatedOn());
     Assertions.assertNotNull(derivative.getId());
   }
 
   @Override
   public void testFind() {
-
+    Derivative result = service.find(Derivative.class, this.derivative.getId());
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(derivative.getUuid(), result.getUuid());
+    Assertions.assertEquals(derivative.getBucket(), result.getBucket());
+    Assertions.assertEquals(derivative.getFileIdentifier(), result.getFileIdentifier());
+    Assertions.assertEquals(derivative.getFileExtension(), result.getFileExtension());
+    Assertions.assertEquals(derivative.getAcHashFunction(), result.getAcHashFunction());
+    Assertions.assertEquals(derivative.getAcHashValue(), result.getAcHashValue());
+    Assertions.assertEquals(derivative.getCreatedBy(), result.getCreatedBy());
+    Assertions.assertEquals(derivative.getCreatedOn(), result.getCreatedOn());
+    Assertions.assertEquals(derivative.getDcType(), result.getDcType());
   }
 
   @Override
