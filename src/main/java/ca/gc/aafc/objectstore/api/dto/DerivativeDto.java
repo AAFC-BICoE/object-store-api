82bde7fced0101b344788d4dce963e3e724aeb8e
Jkeyuk <jonkero2@gmail.com>
2021-03-31 08:58:49 -0400

    22189 derivative repo
    dto fix

--- src/main/java/ca/gc/aafc/objectstore/api/dto/DerivativeDto.java
+++ src/main/java/ca/gc/aafc/objectstore/api/dto/DerivativeDto.java
@@ -3,12 +3,10 @@ package ca.gc.aafc.objectstore.api.dto;
 import ca.gc.aafc.dina.dto.RelatedEntity;
 import ca.gc.aafc.objectstore.api.entities.DcType;
 import ca.gc.aafc.objectstore.api.entities.Derivative;
-import ca.gc.aafc.objectstore.api.entities.ObjectStoreMetadata;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.crnk.core.resource.annotations.JsonApiId;
 import io.crnk.core.resource.annotations.JsonApiRelation;
 import io.crnk.core.resource.annotations.JsonApiResource;
-import lombok.Builder;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
@@ -17,7 +15,6 @@ import java.util.UUID;
 @SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
 @RelatedEntity(Derivative.class)
 @Data
-@Builder
 @JsonApiResource(type = DerivativeDto.TYPENAME)
 public class DerivativeDto {
 
