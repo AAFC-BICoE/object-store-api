d9fd5482f675cd7f56f1c9800891612723dfac1b
Jkeyuk <jonkero2@gmail.com>
2021-04-06 10:58:21 -0400

    22233 update derivatives on metadata
    add test delete_WhenChildDeleted_RelationRemoved

--- src/test/java/ca/gc/aafc/objectstore/api/crud/DerivativeCRUDIT.java
+++ src/test/java/ca/gc/aafc/objectstore/api/crud/DerivativeCRUDIT.java
@@ -3,36 +3,32 @@ package ca.gc.aafc.objectstore.api.crud;
 import ca.gc.aafc.objectstore.api.entities.DcType;
 import ca.gc.aafc.objectstore.api.entities.Derivative;
 import ca.gc.aafc.objectstore.api.entities.ObjectStoreMetadata;
+import ca.gc.aafc.objectstore.api.service.DerivativeService;
+import ca.gc.aafc.objectstore.api.service.ObjectStoreMetaDataService;
 import ca.gc.aafc.objectstore.api.testsupport.factories.ObjectStoreMetadataFactory;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import javax.inject.Inject;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 public class DerivativeCRUDIT extends BaseEntityCRUDIT {
+
+  @Inject
+  private ObjectStoreMetaDataService metaService;
+  @Inject
+  private DerivativeService derivativeService;
 
   private Derivative derivative;
   private final ObjectStoreMetadata metadata = ObjectStoreMetadataFactory.newObjectStoreMetadata().build();
 
   @BeforeEach
   void setUp() {
-    service.save(metadata);
-    derivative = Derivative.builder()
-      .uuid(UUID.randomUUID())
-      .fileIdentifier(UUID.randomUUID())
-      .fileExtension(".jpg")
-      .bucket("mybucket")
-      .acHashValue("abc")
-      .acHashFunction("abcFunction")
-      .dcType(DcType.IMAGE)
-      .createdBy(RandomStringUtils.random(4))
-      .acDerivedFrom(metadata)
-      .derivativeType(Derivative.DerivativeType.THUMBNAIL_IMAGE)
-      .build();
-    service.save(derivative);
+    metaService.create(metadata);
+    derivative = newDerivative(metadata);
+    derivativeService.create(derivative);
   }
 
   @Override
@@ -61,8 +57,40 @@ public class DerivativeCRUDIT extends BaseEntityCRUDIT {
   @Override
   public void testRemove() {
     Integer id = derivative.getId();
-    service.deleteById(Derivative.class, id);
-    assertNull(service.find(Derivative.class, id));
+    derivativeService.delete(derivative);
+    Assertions.assertNull(service.find(Derivative.class, id));
   }
 
+  /**
+   * This test case is required because a child is currently only soft deleted, so we need to establish the
+   * relation is removed.
+   */
+  @Test
+  void delete_WhenChildDeleted_RelationRemoved() {
+    ObjectStoreMetadata child = ObjectStoreMetadataFactory.newObjectStoreMetadata().build();
+    metaService.create(child);
+
+    Derivative parent = newDerivative(child);
+    derivativeService.create(parent);
+
+    metaService.delete(service.find(ObjectStoreMetadata.class, child.getId()));
+    Derivative result = service.find(Derivative.class, parent.getId());
+
+    Assertions.assertNull(result.getAcDerivedFrom());
+  }
+
+  private Derivative newDerivative(ObjectStoreMetadata child) {
+    return Derivative.builder()
+      .uuid(UUID.randomUUID())
+      .fileIdentifier(UUID.randomUUID())
+      .fileExtension(".jpg")
+      .bucket("mybucket")
+      .acHashValue("abc")
+      .acHashFunction("abcFunction")
+      .dcType(DcType.IMAGE)
+      .createdBy(RandomStringUtils.random(4))
+      .acDerivedFrom(child)
+      .derivativeType(Derivative.DerivativeType.THUMBNAIL_IMAGE)
+      .build();
+  }
 }
