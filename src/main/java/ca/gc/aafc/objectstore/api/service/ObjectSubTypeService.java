68a8fced9a50062819bdd9f14dd66b6c4af83296
luusteve <45112541+luusteve@users.noreply.github.com>
2021-04-23 11:36:06 -0400

    Remove column ObjectSubtype.addManaged (#230)
    * Remove column ObjectSubtype.addManaged
    * Fix spotbugs
    * remove objectSubTypeJsonApiIT test

--- src/main/java/ca/gc/aafc/objectstore/api/service/ObjectSubTypeService.java
+++ src/main/java/ca/gc/aafc/objectstore/api/service/ObjectSubTypeService.java
@@ -2,7 +2,6 @@ package ca.gc.aafc.objectstore.api.service;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import ca.gc.aafc.dina.jpa.BaseDAO;
@@ -18,27 +17,6 @@ public class ObjectSubTypeService extends DefaultDinaService<ObjectSubtype> {
   public ObjectSubTypeService(@NonNull BaseDAO baseDAO, MessageSource messageSource) {
     super(baseDAO);
     this.messageSource = messageSource;
-  }
-
-  @Override
-  protected void preCreate(ObjectSubtype entity) {
-    if (entity.isAppManaged()) {
-      throw new AccessDeniedException(getMessage("error.appManaged.create_unsupported"));
-    }
-  }
-
-  @Override
-  protected void preDelete(ObjectSubtype entity) {
-    if (entity.isAppManaged()) {
-      throw new AccessDeniedException(getMessage("error.appManaged.read_only"));
-    }
-  }
-
-  @Override
-  protected void preUpdate(ObjectSubtype entity) {
-    if (entity.isAppManaged()) {
-      throw new AccessDeniedException(getMessage("error.appManaged.update_unsupported"));
-    }
   }
 
   private String getMessage(String key) {
