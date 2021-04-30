0d34810a19ddb8edd77f5be11201671bb0cb537c
Jkeyuk <jonkero2@gmail.com>
2021-04-30 14:31:11 -0400

    22284 validate incoming thumbnails as derivatives
    add validateDerivativeForThumbnail to the validator

--- src/main/java/ca/gc/aafc/objectstore/api/validation/DerivativeValidator.java
+++ src/main/java/ca/gc/aafc/objectstore/api/validation/DerivativeValidator.java
@@ -1,9 +1,12 @@
 package ca.gc.aafc.objectstore.api.validation;
 
 import ca.gc.aafc.objectstore.api.entities.Derivative;
+import ca.gc.aafc.objectstore.api.file.ThumbnailGenerator;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -11,6 +14,9 @@ import org.springframework.validation.Validator;
 @Component
 @RequiredArgsConstructor
 public class DerivativeValidator implements Validator {
+
+  public static final String VALID_THUMB_DC_FORMAT_KEY = "";
+  public static final String VALID_THUMB_SUPPORTED_KEY = "";
 
   private final MessageSource messageSource;
 
@@ -28,5 +34,28 @@ public class DerivativeValidator implements Validator {
 
     Derivative entity = (Derivative) o;
 
+    validateDerivativeForThumbnail(errors, entity);
+  }
+
+  private void validateDerivativeForThumbnail(@NonNull Errors errors, @NonNull Derivative entity) {
+    boolean isDerivativeForThumbnail = entity.getDerivativeType() != null
+      && entity.getDerivativeType() == Derivative.DerivativeType.THUMBNAIL_IMAGE;
+
+    if (isDerivativeForThumbnail) {
+      if (StringUtils.isBlank(entity.getDcFormat())) {
+        loadErrorMessageForKey(errors, VALID_THUMB_DC_FORMAT_KEY);
+      } else {
+        if (!ThumbnailGenerator.isSupported(entity.getDcFormat())) {
+          loadErrorMessageForKey(errors, VALID_THUMB_SUPPORTED_KEY);
+        }
+      }
+    }
+  }
+
+  private void loadErrorMessageForKey(Errors errors, String key) {
+    errors.reject(key, messageSource.getMessage(
+      VALID_THUMB_DC_FORMAT_KEY,
+      null,
+      LocaleContextHolder.getLocale()));
   }
 }
