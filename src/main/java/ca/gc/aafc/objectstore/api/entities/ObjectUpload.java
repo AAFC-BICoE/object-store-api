dda87231b0d7b854781dc78a4f5609e40d28599f
luusteve <luu.stevenluu@gmail.com>
2021-05-06 14:07:01 -0400

    update to springboot 2.5.4 and dina-base 0.52

--- src/main/java/ca/gc/aafc/objectstore/api/entities/ObjectUpload.java
+++ src/main/java/ca/gc/aafc/objectstore/api/entities/ObjectUpload.java
@@ -19,6 +19,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -63,6 +64,12 @@ public class ObjectUpload implements DinaEntity {
 
   public void setId(Integer id) {
     this.id = id;
+  }
+
+  @Override
+  @Transient
+  public UUID getUuid() {
+    return fileIdentifier;
   }
 
   @NaturalId
