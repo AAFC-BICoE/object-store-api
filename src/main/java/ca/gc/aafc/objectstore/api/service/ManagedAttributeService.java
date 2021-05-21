8689dfccdb62fde26b219f1101fc4270eb0f7287
Jkeyuk <jonkero2@gmail.com>
2021-05-21 10:57:35 -0400

    22522 replace manage attribute implementation
    re add ManagedAttributeValidator

--- src/main/java/ca/gc/aafc/objectstore/api/service/ManagedAttributeService.java
+++ src/main/java/ca/gc/aafc/objectstore/api/service/ManagedAttributeService.java
@@ -3,6 +3,7 @@ package ca.gc.aafc.objectstore.api.service;
 import ca.gc.aafc.dina.jpa.BaseDAO;
 import ca.gc.aafc.dina.service.DefaultDinaService;
 import ca.gc.aafc.objectstore.api.entities.ManagedAttribute;
+import ca.gc.aafc.objectstore.api.validation.ManagedAttributeValidator;
 import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
@@ -14,19 +15,31 @@ import java.util.UUID;
 @Service
 public class ManagedAttributeService extends DefaultDinaService<ManagedAttribute> {
 
-  public ManagedAttributeService(@NonNull BaseDAO baseDAO) {
+  private final ManagedAttributeValidator managedAttributeValidator;
+
+  public ManagedAttributeService(
+    @NonNull BaseDAO baseDAO,
+    @NonNull ManagedAttributeValidator managedAttributeValidator
+  ) {
     super(baseDAO);
+    this.managedAttributeValidator = managedAttributeValidator;
   }
 
   @Override
   protected void preCreate(ManagedAttribute entity) {
     entity.setUuid(UUID.randomUUID());
     cleanDescription(entity);
+    validateManagedAttribute(entity);
   }
 
   @Override
   protected void preUpdate(ManagedAttribute entity) {
     cleanDescription(entity);
+    validateManagedAttribute(entity);
+  }
+
+  public void validateManagedAttribute(ManagedAttribute entity) {
+    validateBusinessRules(entity, managedAttributeValidator);
   }
 
   /**
---
+++ src/main/java/ca/gc/aafc/objectstore/api/validation/ManagedAttributeValidator.java
@@ -0,0 +1,35 @@
+package ca.gc.aafc.objectstore.api.validation;
+
+import ca.gc.aafc.objectstore.api.entities.ManagedAttribute;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class ManagedAttributeValidator implements Validator {
+
+  private final MessageSource messageSource;
+
+  public ManagedAttributeValidator(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
+
+  @Override
+  public boolean supports(Class<?> clazz) {
+    return ManagedAttribute.class.isAssignableFrom(clazz);
+  }
+
+  @Override
+  public void validate(Object target, Errors errors) {
+    ManagedAttribute ma = (ManagedAttribute) target;
+    if (ma.getDescription().isEmpty()) {
+      String errorMessage = messageSource.getMessage("description.isEmpty", null,
+        LocaleContextHolder.getLocale());
+      errors.rejectValue("description", "description.isEmpty", null, errorMessage);
+    }
+
+  }
+
+}
