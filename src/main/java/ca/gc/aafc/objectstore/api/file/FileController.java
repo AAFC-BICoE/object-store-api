943f20b3122fa9331757c67c994f55aef27bc9a6
Christian Gendreau <christiangendreau@gmail.com>
2021-05-28 15:37:39 -0400

    22837 reject archive type removed unused import

--- src/main/java/ca/gc/aafc/objectstore/api/file/FileController.java
+++ src/main/java/ca/gc/aafc/objectstore/api/file/FileController.java
@@ -60,7 +60,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
