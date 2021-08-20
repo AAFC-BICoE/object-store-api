bfb56af7dbdf7f023d979722d2ecbce0b37dd68f
Jkeyuk <jonkero2@gmail.com>
2021-08-20 11:04:21 -0400

    24206_remove_orphan_object_uploads
    ObjectUploadResourceRepository will use the objectUploadService not the orphan service

--- src/main/java/ca/gc/aafc/objectstore/api/repository/ObjectUploadResourceRepository.java
+++ src/main/java/ca/gc/aafc/objectstore/api/repository/ObjectUploadResourceRepository.java
@@ -2,9 +2,9 @@ package ca.gc.aafc.objectstore.api.repository;
 
 import ca.gc.aafc.dina.mapper.DinaMapper;
 import ca.gc.aafc.dina.repository.ReadOnlyDinaRepository;
-import ca.gc.aafc.dina.service.DinaService;
 import ca.gc.aafc.objectstore.api.dto.ObjectUploadDto;
 import ca.gc.aafc.objectstore.api.entities.ObjectUpload;
+import ca.gc.aafc.objectstore.api.service.ObjectUploadService;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +13,7 @@ public class ObjectUploadResourceRepository
   extends ReadOnlyDinaRepository<ObjectUploadDto, ObjectUpload> {
 
   protected ObjectUploadResourceRepository(
-    DinaService<ObjectUpload> service,
+    ObjectUploadService service,
     BuildProperties props
   ) {
     super(
