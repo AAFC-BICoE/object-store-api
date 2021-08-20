76f054f4da2d658b0656d52b0fbc028ef640b9f2
Jkeyuk <jonkero2@gmail.com>
2021-08-20 13:01:23 -0400

    24206_remove_orphan_object_uploads
    ObjectOrphanRemovalService add fields
      private final ObjectUploadService objectUploadService;
      private final MinioFileService fileService;

--- src/main/java/ca/gc/aafc/objectstore/api/service/ObjectOrphanRemovalService.java
+++ src/main/java/ca/gc/aafc/objectstore/api/service/ObjectOrphanRemovalService.java
@@ -1,24 +1,19 @@
 package ca.gc.aafc.objectstore.api.service;
 
-import ca.gc.aafc.dina.jpa.BaseDAO;
-import ca.gc.aafc.dina.service.DefaultDinaService;
 import ca.gc.aafc.objectstore.api.entities.ObjectUpload;
-import lombok.NonNull;
+import ca.gc.aafc.objectstore.api.minio.MinioFileService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.SmartValidator;
 
 import java.util.Collections;
 import java.util.List;
 
 @Service
-public class ObjectOrphanRemovalService extends DefaultDinaService<ObjectUpload> {
+@RequiredArgsConstructor
+public class ObjectOrphanRemovalService {
 
-  public ObjectOrphanRemovalService(
-    @NonNull BaseDAO baseDAO,
-    @NonNull SmartValidator validator
-  ) {
-    super(baseDAO, validator);
-  }
+  private final ObjectUploadService objectUploadService;
+  private final MinioFileService fileService;
 
   public void removeObjectOrphans() {
     List<ObjectUpload> orphans = findOrphans();
