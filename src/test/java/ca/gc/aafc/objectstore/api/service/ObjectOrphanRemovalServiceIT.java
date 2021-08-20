771ba0aebe7bbdb09be13321bfd41dfc5e61d20e
Jkeyuk <jonkero2@gmail.com>
2021-08-20 11:31:53 -0400

    24206_remove_orphan_object_uploads
    add test stubs

--- src/test/java/ca/gc/aafc/objectstore/api/service/ObjectOrphanRemovalServiceIT.java
+++ src/test/java/ca/gc/aafc/objectstore/api/service/ObjectOrphanRemovalServiceIT.java
@@ -2,18 +2,21 @@ package ca.gc.aafc.objectstore.api.service;
 
 import ca.gc.aafc.objectstore.api.BaseIntegrationTest;
 import ca.gc.aafc.objectstore.api.entities.ObjectUpload;
-import ca.gc.aafc.objectstore.api.exif.ExifParser;
 import ca.gc.aafc.objectstore.api.minio.MinioFileService;
 import ca.gc.aafc.objectstore.api.minio.MinioTestContainerInitializer;
 import ca.gc.aafc.objectstore.api.testsupport.factories.ObjectUploadFactory;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ContextConfiguration;
 
 import javax.inject.Inject;
-import java.util.Map;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 @ContextConfiguration(initializers = MinioTestContainerInitializer.class)
 class ObjectOrphanRemovalServiceIT extends BaseIntegrationTest {
+
+  public static final String BUCKET = "bucket";
 
   @Inject
   private ObjectOrphanRemovalService serviceUnderTest;
@@ -24,14 +27,37 @@ class ObjectOrphanRemovalServiceIT extends BaseIntegrationTest {
   @Inject
   private MinioFileService fileService;
 
+  @SneakyThrows
   @Test
-  void removeOrphans() {
-    ObjectUpload upload = objectUploadService.create( ObjectUploadFactory.newObjectUpload()
-      .exif(Map.of(ExifParser.DATE_TAKEN_POSSIBLE_TAGS.get(0), "2020:11:13 10:03:17"))
-      .build());
+  void removeOrphans_WhenOrphan_OrphanRemoved() {
+    fileService.ensureBucketExists(BUCKET);
+
+    ObjectUpload upload = ObjectUploadFactory.newObjectUpload().build();
+    upload.setBucket(BUCKET);
+    upload = objectUploadService.create(upload);
+
+    fileService.storeFile(
+      upload.getFileIdentifier().toString() + upload.getEvaluatedFileExtension(),
+      new ByteArrayInputStream("".getBytes(StandardCharsets.UTF_8)),
+      upload.getEvaluatedMediaType(),
+      BUCKET,
+      upload.getIsDerivative());
 
 
+  }
 
+  @Test
+  void removeOrphans_WhenAgeLessThen() {
+
+  }
+
+  @Test
+  void removeOrphans_WhenLinkedToMetadata() {
+
+  }
+
+  @Test
+  void removeOrphans_WhenLinkedToDerivative() {
 
   }
 
