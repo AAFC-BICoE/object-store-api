a70d90e03fed3f8e25c5264468131109a75f5c37
Jkeyuk <jonkero2@gmail.com>
2021-08-20 10:54:32 -0400

    24206_remove_orphan_object_uploads
    Add ObjectOrphanRemovalService.java stubs

---
+++ src/main/java/ca/gc/aafc/objectstore/api/service/ObjectOrphanRemovalService.java
@@ -0,0 +1,48 @@
+package ca.gc.aafc.objectstore.api.service;
+
+import ca.gc.aafc.dina.jpa.BaseDAO;
+import ca.gc.aafc.dina.service.DefaultDinaService;
+import ca.gc.aafc.objectstore.api.entities.ObjectUpload;
+import lombok.NonNull;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.SmartValidator;
+
+import java.util.List;
+
+@Service
+public class ObjectOrphanRemovalService extends DefaultDinaService<ObjectUpload> {
+
+  public ObjectOrphanRemovalService(
+    @NonNull BaseDAO baseDAO,
+    @NonNull SmartValidator validator
+  ) {
+    super(baseDAO, validator);
+  }
+
+  public void removeObjectOrphans() {
+    List<ObjectUpload> orphans = findOrphans();
+    orphans.forEach(objectUpload -> {
+      if (checkAge(objectUpload)) {
+        deleteUpload(objectUpload);
+        deleteMinioFile(objectUpload);
+      }
+    });
+  }
+
+  private List<ObjectUpload> findOrphans() {
+    return null;
+  }
+
+  private boolean checkAge(ObjectUpload objectUpload) {
+    return false;
+  }
+
+  private void deleteUpload(ObjectUpload objectUpload) {
+
+  }
+
+  private void deleteMinioFile(ObjectUpload objectUpload) {
+
+  }
+
+}
