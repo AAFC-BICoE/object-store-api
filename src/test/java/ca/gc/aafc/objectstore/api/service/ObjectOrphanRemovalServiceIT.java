b4b38dd63409399ee7efd9cd9189f7102e18b3fd
Jkeyuk <jonkero2@gmail.com>
2021-08-27 12:53:42 -0400

    24206_remove_orphan_object_uploads
    fill in test case for removeOrphans_WhenOrphanAndAgeLessThen_OrphanNotRemoved

--- src/test/java/ca/gc/aafc/objectstore/api/service/ObjectOrphanRemovalServiceIT.java
+++ src/test/java/ca/gc/aafc/objectstore/api/service/ObjectOrphanRemovalServiceIT.java
@@ -7,12 +7,14 @@ import ca.gc.aafc.objectstore.api.minio.MinioTestContainerInitializer;
 import ca.gc.aafc.objectstore.api.testsupport.factories.ObjectUploadFactory;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ContextConfiguration;
 
 import javax.inject.Inject;
 import javax.persistence.criteria.Predicate;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @ContextConfiguration(initializers = MinioTestContainerInitializer.class)
@@ -29,29 +31,26 @@ class ObjectOrphanRemovalServiceIT extends BaseIntegrationTest {
   @Inject
   private MinioFileService fileService;
 
+  @BeforeEach
+  void setUp() throws IOException {
+    fileService.ensureBucketExists(BUCKET);
+  }
+
   @SneakyThrows
   @Test
-  void removeOrphans_WhenOrphan_OrphanRemoved() {
-    fileService.ensureBucketExists(BUCKET);
-
+  void removeOrphans_WhenOrphanAndOlderThen2Weeks_OrphanRemoved() {
     service.runInNewTransaction(em -> {
       ObjectUpload upload = ObjectUploadFactory.newObjectUpload().build();
       upload.setBucket(BUCKET);
       em.persist(upload);
       em.createNativeQuery("UPDATE object_upload SET created_on = created_on - interval '3 weeks'")
-        .executeUpdate(); // Mock record created in the past
+        .executeUpdate(); // Mock record created in the past and orphan
     });
 
     ObjectUpload upload = objectUploadService.findAll(ObjectUpload.class,
       (criteriaBuilder, objectUploadRoot) -> new Predicate[]{}, null, 0, 10).get(0);
 
-    String fileName = upload.getFileIdentifier().toString() + upload.getEvaluatedFileExtension();
-    fileService.storeFile(
-      fileName,
-      new ByteArrayInputStream("".getBytes(StandardCharsets.UTF_8)),
-      upload.getEvaluatedMediaType(),
-      BUCKET,
-      upload.getIsDerivative());
+    String fileName = storeFileForUpload(upload);
 
     serviceUnderTest.removeObjectOrphans(); // method under test
 
@@ -63,9 +62,23 @@ class ObjectOrphanRemovalServiceIT extends BaseIntegrationTest {
       "There should be no upload record");
   }
 
+  @SneakyThrows
   @Test
-  void removeOrphans_WhenAgeLessThen() {
+  void removeOrphans_WhenOrphanAndAgeLessThen_OrphanNotRemoved() {
+    ObjectUpload upload = ObjectUploadFactory.newObjectUpload().build();
+    upload.setBucket(BUCKET);
+    upload = objectUploadService.create(upload); // record will have current date and is orphan
 
+    String fileName = storeFileForUpload(upload);
+
+    serviceUnderTest.removeObjectOrphans(); // method under test
+
+    Assertions.assertTrue(
+      fileService.getFile(fileName, BUCKET, false).isPresent(),
+      "There should be a returned file");
+    Assertions.assertNotNull(
+      objectUploadService.findOne(upload.getFileIdentifier(), ObjectUpload.class),
+      "There should be a upload record");
   }
 
   @Test
@@ -78,4 +91,15 @@ class ObjectOrphanRemovalServiceIT extends BaseIntegrationTest {
 
   }
 
+  @SneakyThrows
+  private String storeFileForUpload(ObjectUpload upload) {
+    String fileName = upload.getFileIdentifier().toString() + upload.getEvaluatedFileExtension();
+    fileService.storeFile(
+      fileName,
+      new ByteArrayInputStream("".getBytes(StandardCharsets.UTF_8)),
+      upload.getEvaluatedMediaType(),
+      BUCKET,
+      upload.getIsDerivative());
+    return fileName;
+  }
 }
