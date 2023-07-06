9b494e6bd76ae6da140e57df55feeeed83efdbab
Christian Gendreau <christiangendreau@gmail.com>
2023-07-06 16:02:10 -0400

    Completed end2end test of metadata service

--- src/test/java/ca/gc/aafc/objectstore/api/service/MetadataServiceIT.java
+++ src/test/java/ca/gc/aafc/objectstore/api/service/MetadataServiceIT.java
@@ -2,6 +2,7 @@ package ca.gc.aafc.objectstore.api.service;
 
 import org.apache.tika.mime.MimeTypeException;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
@@ -9,14 +10,19 @@ import org.springframework.test.context.ContextConfiguration;
 
 import ca.gc.aafc.objectstore.api.BaseIntegrationTest;
 import ca.gc.aafc.objectstore.api.DinaAuthenticatedUserConfig;
+import ca.gc.aafc.objectstore.api.config.AsyncOverrideConfig;
 import ca.gc.aafc.objectstore.api.dto.ObjectUploadDto;
+import ca.gc.aafc.objectstore.api.entities.Derivative;
 import ca.gc.aafc.objectstore.api.entities.ObjectStoreMetadata;
 import ca.gc.aafc.objectstore.api.file.FileController;
+import ca.gc.aafc.objectstore.api.minio.MinioFileService;
 import ca.gc.aafc.objectstore.api.minio.MinioTestContainerInitializer;
 import ca.gc.aafc.objectstore.api.testsupport.factories.MultipartFileFactory;
 import ca.gc.aafc.objectstore.api.testsupport.factories.ObjectStoreMetadataFactory;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.minio.errors.ErrorResponseException;
 import io.minio.errors.InsufficientDataException;
@@ -25,11 +31,14 @@ import io.minio.errors.InvalidResponseException;
 import io.minio.errors.ServerException;
 import io.minio.errors.XmlParserException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
 import javax.inject.Inject;
 
 @ContextConfiguration(initializers = MinioTestContainerInitializer.class)
+@Import(AsyncOverrideConfig.class)
 public class MetadataServiceIT extends BaseIntegrationTest {
 
   private static final String TEST_UPLOAD_FILE_NAME = "drawing.png";
@@ -39,6 +48,9 @@ public class MetadataServiceIT extends BaseIntegrationTest {
 
   @Inject
   private FileController fileController;
+
+  @Inject
+  private MinioFileService minioFileService;
 
   @Test
   public void endToEndMetadataServiceTest()
@@ -63,6 +75,19 @@ public class MetadataServiceIT extends BaseIntegrationTest {
       .build();
     objectStoreMetaDataService.create(osm);
 
+    // 3 - Make sure a thumbnail is generated
+    Optional<Derivative> thumbnail = derivativeService.findThumbnailDerivativeForMetadata(osm);
+    assertTrue(thumbnail.isPresent());
+
+    // 4 - Make sure we can load the file
+    String thumbnailFilename = thumbnail.get().getFileIdentifier() + thumbnail.get().getFileExtension();
+    Optional<InputStream> file = minioFileService.getFile(thumbnailFilename, DinaAuthenticatedUserConfig.TEST_BUCKET, true);
+    assertTrue(file.isPresent());
+
+    // Deleting the metadata should also delete the derivative and the system generated thumbnail
+    objectStoreMetaDataService.delete(osm);
+    file = minioFileService.getFile(thumbnailFilename, DinaAuthenticatedUserConfig.TEST_BUCKET, true);
+    assertFalse(file.isPresent());
   }
 
 }
