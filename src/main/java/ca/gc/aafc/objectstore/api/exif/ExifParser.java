756adc301873df8dd457ae0db050278da26ebc3c
Christian Gendreau <christiangendreau@gmail.com>
2023-07-10 14:46:18 -0400

    31407 Improve EXIF extraction for NEF Changed ExifParser to take all ExifSubIFDDirectory

--- src/main/java/ca/gc/aafc/objectstore/api/exif/ExifParser.java
+++ src/main/java/ca/gc/aafc/objectstore/api/exif/ExifParser.java
@@ -2,12 +2,15 @@ package ca.gc.aafc.objectstore.api.exif;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
 
@@ -29,6 +32,7 @@ import java.util.stream.Collectors;
 public final class ExifParser {
 
   public static final DateTimeFormatter EXIF_DATE_FORMATTER = DateTimeFormatter.ofPattern("uuuu:MM:dd HH:mm:ss");
+  public static final String UNKNOWN_TAG = "Unknown tag";
   public static final List<String> DATE_TAKEN_POSSIBLE_TAGS = List.of(
       "Date/Time Original",
       "Date/Time",
@@ -40,7 +44,7 @@ public final class ExifParser {
   /**
    * Try to extract EXIF data from the provided {@link InputStream}.
    * If the format is unsupported and empty map is returned.
-   * @param inputStream {@link InputStream} to parse, can't ne null, won't be closed.
+   * @param inputStream {@link InputStream} to parse, can't be null, won't be closed.
    * @return all exif found returned as key/value or empty map if can't be extracted.
    * @throws IOException
    */
@@ -53,12 +57,22 @@ public final class ExifParser {
       return Collections.emptyMap();
     }
 
-    ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-    if (directory == null) {
+    // we need to take ALL the directories of ExifSubIFDDirectory (not only the first one)
+    List<ExifSubIFDDirectory> directories = List.copyOf(metadata.getDirectoriesOfType(ExifSubIFDDirectory.class));
+    if (directories.isEmpty()) {
       return Collections.emptyMap();
     }
-    return directory.getTags().stream().filter(tag -> StringUtils.isNotBlank(tag.getDescription()))
-      .collect(Collectors.toMap(Tag::getTagName, Tag::getDescription));
+
+    Map<String, String> exif = new HashMap<>();
+    for (Directory directory : directories) {
+      for (Tag tag : directory.getTags()) {
+        // skip empty values and Unknown tags
+        if (StringUtils.isNotBlank(tag.getDescription()) && !tag.getTagName().startsWith(UNKNOWN_TAG)) {
+          exif.put(tag.getTagName(), tag.getDescription());
+        }
+      }
+    }
+    return exif;
   }
 
   /**
@@ -78,6 +92,5 @@ public final class ExifParser {
     }
     return Optional.empty();
   }
-
 
 }
