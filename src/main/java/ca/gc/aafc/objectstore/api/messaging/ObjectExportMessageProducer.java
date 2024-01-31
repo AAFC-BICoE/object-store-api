f4abfc39706661e78116df9418cc7731be107100
Christian Gendreau <christiangendreau@gmail.com>
2024-01-31 15:20:33 -0500

    Fixed Checkstyle issue

--- src/main/java/ca/gc/aafc/objectstore/api/messaging/ObjectExportMessageProducer.java
+++ src/main/java/ca/gc/aafc/objectstore/api/messaging/ObjectExportMessageProducer.java
@@ -6,6 +6,5 @@ import ca.gc.aafc.dina.messaging.message.ObjectExportNotification;
  * Interface used to abstract the message producer send method for ({@link ObjectExportNotification}.
  */
 public interface ObjectExportMessageProducer {
-
-   void send(ObjectExportNotification objectExportNotification);
+  void send(ObjectExportNotification objectExportNotification);
 }
