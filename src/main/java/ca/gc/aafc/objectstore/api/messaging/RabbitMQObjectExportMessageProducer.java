1ad0f22eebb62b012a5f3c35a9f6e8d63a7c93bc
Christian Gendreau <christiangendreau@gmail.com>
2024-02-02 13:06:36 -0500

    Added more log

--- src/main/java/ca/gc/aafc/objectstore/api/messaging/RabbitMQObjectExportMessageProducer.java
+++ src/main/java/ca/gc/aafc/objectstore/api/messaging/RabbitMQObjectExportMessageProducer.java
@@ -1,5 +1,7 @@
 package ca.gc.aafc.objectstore.api.messaging;
 
+import lombok.extern.log4j.Log4j2;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import ca.gc.aafc.objectstore.api.config.ObjectExportQueueProperties;
  * RabbitMQ based message producer.
  *
  */
+@Log4j2
 @Service
 @ConditionalOnProperty(prefix = "dina.messaging", name = "isProducer", havingValue = "true")
 public class RabbitMQObjectExportMessageProducer extends RabbitMQMessageProducer<ObjectExportNotification> implements
@@ -19,6 +22,7 @@ public class RabbitMQObjectExportMessageProducer extends RabbitMQMessageProducer
 
   public RabbitMQObjectExportMessageProducer(RabbitTemplate rabbitTemplate, ObjectExportQueueProperties queueProperties) {
     super(rabbitTemplate, queueProperties);
+    log.info( "Using RabbitMQ queue {}", queueProperties::getQueue);
   }
 
 }
