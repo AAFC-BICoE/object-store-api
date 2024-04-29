e1d566b98ef961c4d995cd667ad35cc7991b6e96
Christian Gendreau <christiangendreau@gmail.com>
2024-04-29 15:20:14 -0400

    Use MessageProdcuer from dina-base

--- src/main/java/ca/gc/aafc/objectstore/api/messaging/ObjectExportMessageProducerFallback.java
+++ src/main/java/ca/gc/aafc/objectstore/api/messaging/ObjectExportMessageProducerFallback.java
@@ -5,18 +5,27 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 
-import ca.gc.aafc.dina.messaging.message.ObjectExportNotification;
+import ca.gc.aafc.dina.messaging.DinaMessage;
+import ca.gc.aafc.dina.messaging.message.DocumentOperationNotification;
+import ca.gc.aafc.dina.messaging.producer.DinaMessageProducer;
+import ca.gc.aafc.dina.messaging.producer.DocumentOperationNotificationMessageProducer;
 
 /**
- * Log4j2 based {@link ObjectExportMessageProducer} used mostly to run in dev mode.
+ * Log4j2 based {@link DinaMessageProducer} used mostly to run in dev mode.
  * Used with @ConditionalOnMissingBean as a fallback.
  */
 @Log4j2
 @ConditionalOnMissingBean(RabbitMQObjectExportMessageProducer.class)
 @Service
-public class ObjectExportMessageProducerFallback implements ObjectExportMessageProducer {
+public class ObjectExportMessageProducerFallback implements DinaMessageProducer,
+  DocumentOperationNotificationMessageProducer {
 
-  public void send(ObjectExportNotification documentOperationNotification) {
-    log.info("Message produced : {}", documentOperationNotification::toString);
+  public void send(DinaMessage dinaMessage) {
+    log.info("Message produced : {}", dinaMessage::toString);
+  }
+
+  @Override
+  public void send(DocumentOperationNotification message) {
+    log.info("Message produced : {}", message::toString);
   }
 }
