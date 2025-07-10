fb296cc3461b7520549c7134ff3b7a7049381fd4
Brandon Andre <brandon.andre@agr.gc.ca>
2025-07-10 09:27:13 -0400

    Update MediaTypeDto.java
    Support #36472 - Replace repos extending ReadOnlyResourceRepositoryBase
        
    - Added @JsonIgnore to api-type and api-id

--- src/main/java/ca/gc/aafc/objectstore/api/dto/MediaTypeDto.java
+++ src/main/java/ca/gc/aafc/objectstore/api/dto/MediaTypeDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.apache.tika.mime.MediaType;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.toedter.spring.hateoas.jsonapi.JsonApiId;
 import com.toedter.spring.hateoas.jsonapi.JsonApiTypeForClass;
 
@@ -27,11 +28,13 @@ public class MediaTypeDto implements JsonApiResource {
   }
 
   @Override
+  @JsonIgnore
   public String getJsonApiType() {
     return TYPENAME;
   }
 
   @Override
+  @JsonIgnore
   public UUID getJsonApiId() {
     return null;
   }
