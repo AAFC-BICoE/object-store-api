2086bdb01010deb93fcf5cdaa0cbcf6075c9c5fb
Christian Gendreau <christiangendreau@gmail.com>
2025-07-31 11:14:22 -0400

    36626 Enable bulk load support for object-upload Added bulk-load to object-upload

--- src/main/java/ca/gc/aafc/objectstore/api/repository/ObjectUploadResourceRepository.java
+++ src/main/java/ca/gc/aafc/objectstore/api/repository/ObjectUploadResourceRepository.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ca.gc.aafc.dina.exception.ResourceGoneException;
 import ca.gc.aafc.dina.exception.ResourceNotFoundException;
+import ca.gc.aafc.dina.exception.ResourcesGoneException;
+import ca.gc.aafc.dina.exception.ResourcesNotFoundException;
+import ca.gc.aafc.dina.jsonapi.JsonApiBulkResourceIdentifierDocument;
 import ca.gc.aafc.dina.jsonapi.JsonApiDocument;
 import ca.gc.aafc.dina.repository.DinaRepositoryV2;
 import ca.gc.aafc.dina.service.AuditService;
@@ -56,15 +60,21 @@ public class ObjectUploadResourceRepository extends DinaRepositoryV2<ObjectUploa
   public ResponseEntity<RepresentationModel<?>> onFindOne(@PathVariable UUID id,
                                                           HttpServletRequest req)
       throws ResourceNotFoundException, ResourceGoneException {
-
     return handleFindOne(id, req);
+  }
+
+  @PostMapping(path = ObjectUploadDto.TYPENAME + "/" + DinaRepositoryV2.JSON_API_BULK_LOAD_PATH, consumes = JSON_API_BULK)
+  public ResponseEntity<RepresentationModel<?>> onBulkLoad(@RequestBody
+                                                           JsonApiBulkResourceIdentifierDocument jsonApiBulkDocument,
+                                                           HttpServletRequest req)
+      throws ResourcesNotFoundException, ResourcesGoneException {
+    return handleBulkLoad(jsonApiBulkDocument, req);
   }
 
   @GetMapping(ObjectUploadDto.TYPENAME)
   public ResponseEntity<RepresentationModel<?>> onFindAll(HttpServletRequest req) {
     return handleFindAll(req);
   }
-
 
   @PatchMapping(ObjectUploadDto.TYPENAME + "/{id}")
   @Transactional
