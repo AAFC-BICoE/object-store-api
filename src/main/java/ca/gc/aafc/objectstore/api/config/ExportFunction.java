72a1cf2441661110321cdafbfd78c6754966882e
Christian Gendreau <christiangendreau@gmail.com>
2025-09-15 09:58:35 -0400

    36903 Change ExportFunction to use named params resize function nows required factor named param

--- src/main/java/ca/gc/aafc/objectstore/api/config/ExportFunction.java
+++ src/main/java/ca/gc/aafc/objectstore/api/config/ExportFunction.java
@@ -1,6 +1,6 @@
 package ca.gc.aafc.objectstore.api.config;
 
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 import lombok.Builder;
@@ -13,16 +13,18 @@ import org.springframework.http.MediaType;
  * @param params
  */
 @Builder
-public record ExportFunction(FunctionDef functionDef, List<String> params) {
+public record ExportFunction(FunctionDef functionDef, Map<String, String> params) {
+
+  public static final String IMG_RESIZE_PARAM_FACTOR = "factor";
 
   public enum FunctionDef {
     IMG_RESIZE(Set.of(MediaType.IMAGE_JPEG_VALUE), ExportFunction::imageResizeParamValidator, "resized");
 
     private final Set<String> supportedMediaType;
-    private final Predicate<List<String>> paramsValidator;
+    private final Predicate<Map<String, String>> paramsValidator;
     private final String suffix;
 
-    FunctionDef(Set<String> supportedMediaType, Predicate<List<String>> paramsValidator, String suffix) {
+    FunctionDef(Set<String> supportedMediaType, Predicate<Map<String, String>> paramsValidator, String suffix) {
       this.supportedMediaType = supportedMediaType;
       this.paramsValidator = paramsValidator;
       this.suffix = suffix;
@@ -32,7 +34,7 @@ public record ExportFunction(FunctionDef functionDef, List<String> params) {
       return supportedMediaType.contains(mediaType);
     }
 
-    public boolean areParamsValid(List<String> params) {
+    public boolean areParamsValid(Map<String, String> params) {
       return paramsValidator.test(params);
     }
 
@@ -54,13 +56,13 @@ public record ExportFunction(FunctionDef functionDef, List<String> params) {
     return functionDef.areParamsValid(params);
   }
 
-  private static boolean imageResizeParamValidator(List<String> params) {
-    if (params == null || params.size() != 1) {
+  private static boolean imageResizeParamValidator(Map<String, String> params) {
+    if (params == null || params.size() != 1 || !params.containsKey(IMG_RESIZE_PARAM_FACTOR)) {
       return false;
     }
 
     try {
-      float value = Float.parseFloat(params.getFirst());
+      float value = Float.parseFloat(params.get(IMG_RESIZE_PARAM_FACTOR));
       return value > 0 && value < 1;
     } catch (NumberFormatException e) {
       return false;
