047359aef56e02f7b0e9255547c8af36d6d2038f
Christian Gendreau <christiangendreau@gmail.com>
2026-03-26 10:40:24 -0400

    38055 Add API info endpoint Added new ApiInfo endpoint. admin only

---
+++ src/main/java/ca/gc/aafc/objectstore/api/config/ApiInfoConfiguration.java
@@ -0,0 +1,40 @@
+package ca.gc.aafc.objectstore.api.config;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Configuration;
+
+import ca.gc.aafc.dina.dto.ApiInfoDto;
+
+@Configuration
+public class ApiInfoConfiguration {
+
+  @Value("${dina.fileStorage.implementation:}")
+  private String storageImplementation;
+
+  @Value("${dina.messaging.isProducer:false}")
+  private Boolean isProducer;
+
+  @Value("${dina.messaging.isConsumer:false}")
+  private Boolean isConsumer;
+
+  private final String apiVersion;
+
+  public ApiInfoConfiguration(BuildProperties buildProperties) {
+    this.apiVersion = buildProperties.getVersion();
+  }
+
+  public ApiInfoDto buildApiInfoDto() {
+    ApiInfoDto infoDto = new ApiInfoDto();
+    infoDto.setModuleVersion(apiVersion);
+    infoDto.setMessageProducer(isProducer);
+    infoDto.setMessageConsumer(isConsumer);
+
+    Map<String, Object> moduleInfo = Map.of("storageImplementation", storageImplementation);
+    infoDto.setModuleInfo(moduleInfo);
+    return infoDto;
+  }
+
+}
